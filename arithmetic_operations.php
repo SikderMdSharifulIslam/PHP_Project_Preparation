befc7d3b629c8177e2052c7d0bad50025450fdf9
sikdermdsharifulislam <sikdermdsharifulislam@gmail.com>
2024-01-07 18:03:46 +0600

    arithmetic operations

--- arithmetic_operations.php
+++ arithmetic_operations.php
@@ -1,21 +1,4 @@
 <?php
-    //Print
-    echo"<h1>Hello World of PHP</h1>";
-    echo"<p>This is my first php program.</p>";
-
-    //Variable
-    $var = "Sikder";
-    echo $var;
-    $age = 25;
-    echo $age;
-    echo "<br>";
-    $var1 = "Sikder";
-    $var2 = "Md. Shariful";
-    $var3 = "Islam";
-    echo $var1 ." ". $var2 ." ". $var3. "<br>";
-
-
-    //Arithmetic Operations
     $var1 = 40;
     $var2 = 20;
     $sum = $var1 + $var2; //60
@@ -25,6 +8,7 @@
     echo "Num1 = $var1" . "<br>" . "Num2 = $var2 <br>";
     echo "Sum = $sum" . " Sub = $sub" . " Mul = $mul" . " Div = $div";
 
+    //special case of php
     $string = "123";
     $number = 45;
     echo "<br>". $result = $string + $number; // $result will be 168 (123 + 45)
