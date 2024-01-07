f95ac446cc42fa1474c60eff18e1ddea159c7ad6
sikdermdsharifulislam <sikdermdsharifulislam@gmail.com>
2024-01-07 18:26:03 +0600

    use define in variable

--- variables.php
+++ variables.php
@@ -8,4 +8,15 @@
     $var2 = "Md. Shariful";
     $var3 = "Islam";
     echo $var1 ." ". $var2 ." ". $var3;
+
+    //We can declare and overwrite variables value in php
+    $var1 = 40;
+    $var1 = "Sikder";
+    echo $var1; //output will be Sikder
+
+    //To avoid this phenomena use define keyword
+    define("VARI", "40"); //variable name should be uppercase here.
+    $VARI = "Sikder";
+    echo VARI;
+    $var2 = "20";
 ?>
